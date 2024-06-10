a47f181060bc44f7a26cca38973518b4e05336bd
Olivier Leroy <leroy.oli@proton.me>
2024-06-10 17:13:35 -0400

    listing s3 with paws

---
+++ R/list_s3.R
@@ -0,0 +1,15 @@
+#' Listing s3 bucket CORI draft
+#' 
+
+get_me_s3_table <- function() {
+  s3 <- paws::s3()
+  list_s3  <- s3$list_buckets()
+  bucket <- list_s3[["Buckets"]]
+  s3_dat <- data.frame(bucket_name = sapply(bucket,
+                                            function(x) x[["Name"]]),
+                       creation_date = as.POSIXlt(
+                            sapply(bucket,
+                                    function(x) x[["CreationDate"]]
+                                   )))
+  return(s3_dat)
+}
