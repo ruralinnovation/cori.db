ceb356e7baabb9cb4a236601858711a8e42c0a52
Olivier Leroy <leroy.oli@proton.me>
2024-06-28 11:51:11 -0400

    totally unfished design

---
+++ R/list_bucket.R
@@ -0,0 +1,14 @@
+
+
+s3 <- paws::s3()
+bob <- s3$list_objects(Bucket = "test-coridata")
+
+gimme_me_key <- function(x) {x[["Key"]]}
+gimme_me_last_modified <- function(x) {x[["LastModified"]]}
+
+df_temp <- data.frame(
+    key = do.call(rbind, lapply(bob[["Contents"]], gimme_me_key)),
+    last_modified = as.POSIXlt(do.call(rbind, lapply(bob[["Contents"]], gimme_me_last_modified)))
+)
+
+sapply(strsplit(df_temp$key, "/"), function(x) length(x))
