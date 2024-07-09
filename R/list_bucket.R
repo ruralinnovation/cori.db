5abfe2038c265cbdf63a826035c50f4196938e35
Olivier Leroy <leroy.oli@proton.me>
2024-07-09 12:02:24 -0400

    comment list s3

--- R/list_bucket.R
+++ R/list_bucket.R
@@ -3,7 +3,7 @@
 s3 <- paws::s3()
 bob <- s3$list_objects(Bucket = "test-coridata")
 
-gimme_me_key <- function(x) {x[["Key"]]}
+gimme_me_key <- function(x) x[["Key"]]
 gimme_me_last_modified <- function(x) {x[["LastModified"]]}
 
 df_temp <- data.frame(
@@ -12,3 +12,5 @@ df_temp <- data.frame(
 )
 
 sapply(strsplit(df_temp$key, "/"), function(x) length(x))
+
+dbo.call(rbind, lapply(bucket[["Contents"]]), function(x) x[["Key"]])
