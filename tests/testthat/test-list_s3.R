b7e55692eab9ea226e8924480ae0c7ea8b3c1a2f
Olivier Leroy <31417689+defuneste@users.noreply.github.com>
2024-07-09 11:39:10 -0400

    Update test-list_s3.R
    making the test better again!

--- tests/testthat/test-list_s3.R
+++ tests/testthat/test-list_s3.R
@@ -1,3 +1,7 @@
+# TODO do an if on aws key if not present expect_error
+# if no renviron expect error
+# if Renvron but no aws ket expect error
+
 test_that("list_s3 return a data frame", {
   expect_equal(is.data.frame(list_s3()), TRUE)
 })
