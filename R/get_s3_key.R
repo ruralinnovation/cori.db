d1493d55130926f24f18c742ffa4c0f46a932bff
Olivier Leroy <leroy.oli@proton.me>
2024-07-15 14:59:08 -0400

    resolve namespace conflict

--- R/get_s3_key.R
+++ R/get_s3_key.R
@@ -1,41 +1,43 @@
-#' get key/file hosted in s3 downloaded
+#' get object hosted in s3 downloaded
 #'
-#' @return good question
+#'@details If file_path is not provided it will the object in the curent directory
+#'
+#' @param bucket_name string, a bucket name
+#' @param key string, object/file that you want to download
+#' @param file_path string, a path where you want the file to be downloaded ("~/Documents/my_proj/"")
+#' @param ... other arguments from paws's downloadfile
+#'
+#' @return return invisibly the path where the file has been downloaded
 #'
 #' @export
+#'
 #' @examples
 #'
 #' \dontrun{
-#'  get_s3_key("test-coridata", "blabla.txt", "text.txt")
+#'  txt <- get_s3_key("test-coridata", "blabla.txt")
+#'  txt
 #' }
 #'
 
+get_s3_object <- function(bucket_name, key, file_path, ...) {
 
-get_s3_key <- function(bucket_name, key, file_path, ...) {
+  if(! has_aws_credentials()) stop("AWS credentials are missing, run set_aws_credentials()")
+
+  if (missing(file_path)) {
+    where_to_write <- paste0(getwd(), "/", key)
+  } else {
+    where_to_write <-  paste0(file_path, key)
+  } 
 
   s3 <- paws::s3()
 
   s3$download_file(
     Bucket = bucket_name,
     Key = key,
-    Filename = file_path,
+    Filename = where_to_write,
     ...
   )
+  message(sprintf("%s will be downloaded here: %s", key, where_to_write))
+
+  return(invisible(where_to_write))
 }
-
-
-#  s3$get_object(
-#    Bucket = "test-coridata",
-#    Key = "blabla.txt"
-#  )
-
-# # return $tagCount 1 but do not show it
-
-# s3$get_object_tagging(
-#   Bucket = "test-coridata",
-#   Key = "blabla.txt"
-# )
-
-# s3$get_bucket_tagging(
-#   Bucket = "test-coridata"
-# )
