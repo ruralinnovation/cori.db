bbd98a5eec6057683eac9cf4b21638b61ffe1b36
Olivier Leroy <leroy.oli@proton.me>
2024-12-06 10:05:20 -0500

    adding a begining of test to write to DB

---
+++ tests/testthat/test-write_db.R
@@ -0,0 +1,16 @@
+skip_if_no_renviron <- function() {
+  renviron <- file.path(Sys.getenv("HOME"), ".Renviron")
+  if (!file.exists(renviron)) {
+    skip("No .Renviron")
+  }
+}
+
+test_that("Check if we can write to staging", {
+  skip_if_no_renviron()
+  con <- connect_to_db("staging")
+  DBI::dbWriteTable(con, "mtcars", mtcars, overwrite = TRUE)
+  mtcars_DB <- DBI::dbGetQuery(con, "select * from mtcars")
+  DBI::dbDisconnect(con)
+  expect_equal(nrow(mtcars), nrow(mtcars_DB))
+
+})
