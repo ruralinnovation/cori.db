0b7b98821ae93df3d6e0e81c35a09e0acd827aa1
John Hall <john.hall@ruralinnovation.us>
2024-12-16 20:15:11 -0500

    add copy functions to cori db package

--- R/copy_table_across_databases.R
+++ R/copy_table_across_databases.R
@@ -1,11 +1,18 @@
-
-copy_table_across_databases <- function (schema_name, table_name, from_db, to_db, grant_select_roles) {
+#' Copy a table between two CORI/RISI Postgres DB instances
+#'
+#' @param schema_name schema name in from_db with target table that will be copied to same schema name in to_db
+#' @param from_db database name to copy the target table to
+#' @param to_db database name to copy the target table to
+#' @param table_name name of the table that will be copied
+#' @param grant_select_roles vector of role names that will be granted permission to select table rows (e.g. c(
+#'   "read_only_access",
+#'   "r_team"
+#' ))
+#' @return a data.frame of the copied table
+#' @export
+#'
+copy_table_across_databases <- function (schema_name, table_name, from_db, to_db, grant_select_roles = c("read_only_access")) {
   df <- NULL
-
-  # grant_select_roles <- c(
-  #   "read_only_access",
-  #   "r_team"
-  # )
 
   dest <- paste0('"', schema_name, '"."', table_name, '"')
   df <- (function () {
@@ -21,25 +28,21 @@ copy_table_across_databases <- function (schema_name, table_name, from_db, to_db
     on.exit(DBI::dbDisconnect(con), add = TRUE)
 
     message(paste0("Writing data frame to tableau db as ", dest))
-    # DBI::dbWriteTable(con, table_name, df, overwrite = TRUE)
-    cori.db::write_db(con, table_name, df, overwrite = TRUE)  # TODO: this should take optional
-    #       args to set roles/permissions
+
+    cori.db::write_db(con, table_name, df, overwrite = TRUE)
     tryCatch({
-      message(paste0("Set access permissions on ", dest))
+      message(paste0("Set access permissions on ", dest, " for..."))
       # Grant access to read_only_access group role (for testing):
       sapply(grant_select_roles, function (x) {
+        message(x)
         DBI::dbExecute(con,
                        paste0("GRANT SELECT ON TABLE ", dest, " TO ", x, ";")
         )
       })
-      # DBI::dbExecute(con, paste0("GRANT SELECT ON TABLE ", dest, " TO read_only_access;"))
-      # # Grant access to r_team role:
-      # DBI::dbExecute(con, paste0("GRANT SELECT ON TABLE ", dest, " TO r_team;"))
     }, error = function (e) {
       stop(paste0("Failed to set permissions on ", dest, "\n", e))
     })
 
-    # df <- cori.db::read_db(con, table = table_name) # <= Error: ...
     df <- DBI::dbReadTable(con, table_name)
   }
 
--- R/copy_table_across_schemas.R
+++ R/copy_table_across_schemas.R
@@ -1,11 +1,19 @@
+#' Copy a table from one schema to another within the same CORI/RISI Postgres DB instance
+#' ... will fail if table already exists in to_schema:
+#' ERROR:  relation "..." already exists
+#'
+#' @param from_schema schema name to copy the target table from
+#' @param to_schema schema name to copy the target table to
+#' @param table_name name of the table that will be copied
+#' @return the status of the copy transaction
+#' @export
+#'
+copy_table_across_schemas <- function (from_schema, to_schema, table_name) {
 
-copy_table_across_schemas <- function (schema_name, table_name, to_schema) {
-  result <- NULL
-
+  from <- paste(from_schema, table_name, sep = ".")
   dest <- paste(to_schema, table_name, sep = ".")
-  from <- paste(schema_name, table_name, sep = ".")
 
-  con <- cori.db::connect_to_db(to_schema)
+  con <- cori.db::connect_to_db(c(from_schema, to_schema))
   on.exit(DBI::dbDisconnect(con), add = TRUE)
 
   query_to_create_target <- paste0("CREATE table ", dest, " (like ", from , " including all);")
@@ -25,7 +33,5 @@ copy_table_across_schemas <- function (schema_name, table_name, to_schema) {
   query_to_copy_table_to_target <- paste0("INSERT into ", dest, "
                                               SELECT * FROM ", from, ";")
 
-  result <- DBI::dbExecute(con,  query_to_copy_table_to_target)
-
-  return(invisible(result))
+  return(invisible(DBI::dbExecute(con,  query_to_copy_table_to_target)))
 }
